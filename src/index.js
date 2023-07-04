58b64f1feaa93cad5e2ffcf397c5fd876b1c26b9
mohamed warsame <mwarsame111@gmail.com>
2023-07-04 15:45:21 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const form = document.querySelector('form')
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addToDo(e.target.new_task_description.value);
+    e.target.reset();
+  });
+
+  function addToDo (task) {
+    let p = document.createElement("p")
+    p.textContent = `${task}`
+    document.querySelector("#tasks").appendChild(p)
+  }
 });
